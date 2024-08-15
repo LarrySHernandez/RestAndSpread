44a92b9d90c8aa3e613bc28a0071c546e2f9b048
larry <larryh2612@gmail.com>
2024-08-14 22:13:50 -0400

    Final Version

--- app.js
+++ app.js
@@ -42,48 +42,40 @@ const doubleAndReturnArgs = (arr, ...nums) => arr.concat(nums.map((num) => num *
 /** remove a random element in the items array
 and return a new array without that item. */
 
-let removeRandom = (...items) => {
-    let randomNum = (Math.floor(Math.random() * items.length));
-    return items.filter(function(item) {
-        if(items.indexOf(item) !== randomNum) return item; 
-    })
+let removeRandom = (items) => {
+    let randomNum = Math.floor(Math.random() * items.length);
+    return [...items.slice(0, idx), ...items.slice(idx + 1)];
 };
 
 /** Return a new array with every item in array1 and array2. */
 
-function extend(array1, array2) {
-    console.log('test');
-}
-
-let extend2 = (...arrays) => arrays.map(console.log(arrays));
+let extend = (array1, array2) => ([...array1, ...array2]);
 
 /** Return a new object with all the keys and values
 from obj and a new key/value pair */
 
-function addKeyVal(obj, key, val) {
-
-}
+let addKeyVal = (obj, key, val) => ({...obj, [key]: val});
 
 
 /** Return a new object with a key removed. */
 
-function removeKey(obj, key) {
-
-}
+let removeKey = (obj, key) => {
+    let newObj = {...obj}
+    delete newObj[key];
+    return newObj;
+};
 
 
 /** Combine two objects and return a new object. */
 
-function combine(obj1, obj2) {
-
-}
+let combine = (obj1, obj2) => ({...obj1, ...obj2});
 
 
 /** Return a new object with a modified key and value. */
 
-function update(obj, key, val) {
 
-}
+
+let update = (obj, key, val) => ({...obj, [key]: val});
 
 
 
